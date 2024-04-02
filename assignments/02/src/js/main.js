e523db3e38093d0415b432eceae30ba1a4f3e432
ilyanosovsky <imnosovsky@gmail.com>
2024-04-02 21:21:57 +0300

    sorted task complete

--- assignments/02/src/js/main.js
+++ assignments/02/src/js/main.js
@@ -1,3 +1,26 @@
-import { contacts } from '../data/contacts.js';
+import { contacts } from "../data/contacts.js";
 
 console.log(contacts);
+
+// Array Order Methods
+
+// Write a function that takes the array of contacts and returns a new array sorted by last name.
+
+const sortedByLastName = (contacts) => {
+  const sortedContacts = contacts.slice().sort((a, b) => {
+    const lastNameA = a.name.last.toLowerCase();
+    const lastNameB = b.name.last.toLowerCase();
+
+    if (lastNameA < lastNameB) {
+      return -1; // lastNameA comes before lastNameB
+    }
+    if (lastNameA > lastNameB) {
+      return 1; // lastNameA comes after lastNameB
+    }
+    return 0; // names are equal
+  });
+  return sortedContacts;
+};
+
+console.log(sortedByLastName(contacts.results));
+
