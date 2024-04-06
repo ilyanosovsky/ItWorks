8d80ec095b48b2a88ca69e4a54a8a9e00e54ad7d
ilyanosovsky <imnosovsky@gmail.com>
2024-04-06 14:41:46 +0300

    finished second exersice

--- assignments/04/script.js
+++ assignments/04/script.js
@@ -17,7 +17,35 @@ const solveBasicOperations = new Promise((resolve, reject) => {
     return (num -= 2);
   })
   .then((num) => {
-    console.log("finale result ->", num);
+    console.log("finale result of Basic Arithmetic Operations ->", num);
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+  });
+
+//   Exercise 2: String Manipulation
+//   Write a promise chain that takes a string, converts it to uppercase,
+//   then reverses it, and finally appends the string "-finished" at the end.
+//   Log the final result.
+//   Use `then` for every phase
+
+const solveStringManipulation = new Promise((resolve, reject) => {
+  const initialString = "test string";
+  resolve(initialString);
+})
+  .then((str) => {
+    return str.toUpperCase();
+  })
+  .then((str) => {
+    console.log(str);
+    return str.split("").reverse().join("");
+  })
+  .then((str) => {
+    console.log(str);
+    return str + " - finished";
+  })
+  .then((str) => {
+    console.log("finale result of String Manipulation ->", str);
   })
   .catch((error) => {
     console.error("Error occurred:", error);
