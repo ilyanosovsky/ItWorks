8b5b8b6c8e232c51ce4b404c4c5454ff63dc9d3d
ilyanosovsky <imnosovsky@gmail.com>
2024-04-13 14:08:42 +0300

    helpers and five day forecast

---
+++ assignments/herolo-app/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, createContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import FiveDaysForecast from "./components/FiveDaysForecast";
+
+export const AppContext = createContext(null);
+
+function App() {
+  const [city, setCity] = useState('Tel Aviv');
+  const [country, setCountry] = useState('Israel');
+  const [cityKey,setCityKey] = useState(215854);
+  const [metric, setMetric] = useState(true)
+  
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AppContext.Provider
+              value={{
+                city,
+                setCity,
+                cityKey,
+                setCityKey,
+                metric,
+                setMetric,
+                country,
+                setCountry,
+              }}
+            >
+              <div className="App">
+                <FiveDaysForecast />
+              </div>
+            </AppContext.Provider>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
