1712f6f5dc4b11cb5bed6ea87af23901fefbaed4
ilyanosovsky <imnosovsky@gmail.com>
2024-04-16 00:51:24 +0300

    test with postman

--- exercises/week-7/02/index.js
+++ exercises/week-7/02/index.js
@@ -1,7 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
-// import cors from "cors";
 import dotenv from "dotenv";
 import authorRoutes from './routes/authors.js';
 import bookRoutes from './routes/books.js';
@@ -9,8 +7,6 @@ import bookRoutes from './routes/books.js';
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 //ROUTES
 app.use("/authors", authorRoutes);
@@ -19,10 +15,7 @@ app.use("/books", bookRoutes);
 // MONGOOSE - MongoDB SetUp
 const PORT = process.env.PORT || 3030;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
   })
