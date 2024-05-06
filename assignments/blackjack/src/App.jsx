e56c19b893d4b630b1cd4e5af5c6b8d762e1b8ff
ilyanosovsky <imnosovsky@gmail.com>
2024-05-06 22:29:53 +0300

    start blackjack

--- assignments/blackjack/src/App.jsx
+++ assignments/blackjack/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Controls from "./components/Controls";
+import Desk from "./components/Desk";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const STATUSES = {
+  inProgress: "In Progress",
+  stopped: "Stopped",
+  playerWin: "Player WIN",
+  playerLoose: "Player LOOSE",
+};
 
+const buildDeck = () => {
+  const fullSuites = ["♠", "♥", "♦", "♣"];
+
+  const deck = [];
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 1; j <= 13; j++) {
+      deck.push({ type: fullSuites[i], value: j });
+    }
+  }
+
+  return deck;
+};
+
+// a fn thats shuffles the deck
+const shuffleDeck = (deck) => {
+  return deck.sort(() => Math.random() - 0.5);
+};
+
+const App = () => {
+  const [deck, setDeck] = useState(shuffleDeck(buildDeck()));
+  const [deckPlayer, setDeckPlayer] = useState([]);
+  const [deckDealer, setDeckDealer] = useState([]);
+  const [gameStatus, setGameStatus] = useState(STATUSES.inProgress);
+
+  function handleDeal() {
+    if (gameStatus != STATUSES.inProgress) return;
+    const deckCopy = [...deck];
+    const card = deckCopy.pop();
+    setDeck(deckCopy);
+    setDeckPlayer([...deckPlayer, card]);
+    console.log("deal", deckPlayer);
+  }
+
+  function handleStop() {
+    if (gameStatus != STATUSES.inProgress) return;
+    setGameStatus(STATUSES.stopped);
+    setDeckDealer([deck[0], deck[1]]);
+    console.log("stop", deckPlayer, deckDealer);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Desk status={gameStatus} />
+      <Controls handleDeal={handleDeal} handleStop={handleStop} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
