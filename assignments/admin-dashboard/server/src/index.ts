a8da6ec35763cdc06d4b330c21743f6ce8c1dc1a
ilyanosovsky <imnosovsky@gmail.com>
2024-05-10 18:10:18 +0300

    added connection to db

--- assignments/admin-dashboard/server/src/index.ts
+++ assignments/admin-dashboard/server/src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to DB"));
 
 const app = express();
 app.use(express.json());
@@ -11,6 +16,5 @@ app.get("/test", async (req: Request, res: Response) => {
 });
 
 app.listen(5001, () => {
-    console.log("server started on Port 5001");
-    
-})
+  console.log("server started on Port 5001");
+});
