0e9ced1d0af1cb7cc3e249b29a66174bd1d8d4bf
ilyanosovsky <imnosovsky@gmail.com>
2024-05-10 22:35:08 +0300

    created some routes and layout

---
+++ assignments/admin-dashboard/client/src/AppRoutes.tsx
@@ -0,0 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout>HOME PAGE</Layout>} />
+      <Route path="/login" element={<span>LOGIN PAGE</span>} />
+      <Route path="/view-users" element={<span>View Users</span>} />
+      <Route path="/create-user" element={<span>Create User</span>} />
+      <Route path="/edit-user" element={<span>Edit User</span>} />
+      <Route path="/logout" element={<span>LOGOUT</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
