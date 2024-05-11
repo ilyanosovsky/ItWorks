7c6acc01b63bf0034a870b20456af15a518aa0da
ilyanosovsky <imnosovsky@gmail.com>
2024-05-11 13:00:07 +0300

    added home page and footer

--- assignments/admin-dashboard/client/src/AppRoutes.tsx
+++ assignments/admin-dashboard/client/src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout>HOME PAGE</Layout>} />
+      <Route path="/" element={<Layout><HomePage/></Layout>} />
       <Route path="/login" element={<span>LOGIN PAGE</span>} />
       <Route path="/view-users" element={<span>View Users</span>} />
       <Route path="/create-user" element={<span>Create User</span>} />
