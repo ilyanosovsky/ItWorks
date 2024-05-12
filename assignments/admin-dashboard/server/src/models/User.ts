74567c5a46405c1dae298a55365a48ba3194bc4d
ilyanosovsky <imnosovsky@gmail.com>
2024-05-12 20:23:10 +0300

    added models and structure files

---
+++ assignments/admin-dashboard/server/src/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
